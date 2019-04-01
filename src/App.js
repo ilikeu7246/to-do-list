07363886f0ba05e9af299282a5fb60804c7c5a7f
jinsun <wlstjs7246@naver.com>
2019-04-01 22:13:32 +0900

    6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import TodoListTemplate from "./components/TodoListTemplate";
+import Form from "./components/Form";
 
 class App extends Component {
   render() {
     return (
-        <TodoListTemplate form={<div>이렇게 말이죠.</div>}>
+        <TodoListTemplate form={<Form />}>
           <div>여기엔 children 자리구요.</div>
         </TodoListTemplate>
     );
